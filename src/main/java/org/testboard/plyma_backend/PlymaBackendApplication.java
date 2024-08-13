bdeb4c235cd26322d8e003912aa6a3ee9333ad29
rudwk <tlsruwk12345@gmail.com>
2024-08-13 18:51:44 +0900

    ⚙️ :: 프로젝트 세팅

---
+++ src/main/java/org/testboard/plyma_backend/PlymaBackendApplication.java
@@ -0,0 +1,13 @@
+package org.testboard.plyma_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlymaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlymaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/testboard/plyma_backend/PlymaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.testboard.plyma_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlymaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
