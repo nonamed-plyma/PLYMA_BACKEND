35107468bd9cd745370181f6a12d6ed1c08edf11
rudwk <tlsruwk12345@gmail.com>
2024-08-23 16:55:44 +0900

    📑 :: Auth 추가

---
+++ src/main/java/org/testboard/plyma_backend/global/jwt/auth/AuthDetails.java
@@ -0,0 +1,40 @@
+package org.testboard.plyma_backend.global.jwt.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class AuthDetails implements UserDetails {
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){return null;}
+
+    @Override
+    public String getPassword(){return user.getPassword();}//password가져오기
+
+    @Override
+    public String getUsername(){return user.getUsername();}//Username가져오기
+
+    @Override
+    public boolean isAccountNonExpired(){return true;}//계정 비활성화 되어있지 않는가?
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }//계정이 잠겨있지 않은가?
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }//자격이 만료되었는가
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }//활성화 되어있는가?
+}
