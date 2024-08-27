4416cb10d4d28f4c9f37556b60cba20bd4ac7c81
rudwk <tlsruwk12345@gmail.com>
2024-08-27 22:33:20 +0900

    📑 :: reassignToken추가

--- src/main/java/org/testboard/plyma_backend/domain/user/service/ReassignTokenService.java
+++ src/main/java/org/testboard/plyma_backend/domain/user/service/ReassignTokenService.java
@@ -1,2 +1,38 @@
-package org.testboard.plyma_backend.domain.user.service;public class ReassignTokenService {
+package org.testboard.plyma_backend.domain.user.service;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import org.testboard.plyma_backend.domain.user.exception.UserNotFoundException;
+import org.testboard.plyma_backend.domain.user.presentation.dto.TokenResponse;
+import org.testboard.plyma_backend.global.jwt.JwtTokenProvider;
+
+@Service
+@RequiredArgsConstructor
+public class ReassignTokenService {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserDetailsService userDetailsService;
+
+    public TokenResponse reasSignToken(String refreshToken){
+        if (!jwtTokenProvider.validateToken(refreshToken)) {
+            throw UserNotFoundException.EXCEPTION;
+        }
+        String userId = jwtTokenProvider.getSubject(refreshToken);
+        UserDetails userDetails;
+        try{
+            userDetails = userDetailsService.loadUserByUsername(userId);
+        }catch (UserNotFoundException exception){
+            throw UserNotFoundException.EXCEPTION;
+        }
+        String newAccessToken = jwtTokenProvider.generateAccessToken(userId);
+        TokenResponse response = TokenResponse.builder()
+                .accessToken(newAccessToken)
+                .refreshToken(refreshToken)
+                .build();
+
+        return response;
+    }
 }
