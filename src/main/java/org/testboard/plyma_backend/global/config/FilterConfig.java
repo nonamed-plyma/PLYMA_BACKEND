7097309380ae52a2cb2744187703f69c29989bb8
rudwk <tlsruwk12345@gmail.com>
2024-11-12 19:44:59 +0900

    📑 :: config 추가

--- src/main/java/org/testboard/plyma_backend/global/config/FilterConfig.java
+++ src/main/java/org/testboard/plyma_backend/global/config/FilterConfig.java
@@ -5,8 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.testboard.plyma_backend.global.jwt.JwtTokenFilter;
+import org.testboard.plyma_backend.global.jwt.JwtTokenProvider;
 
 @Configuration
 @RequiredArgsConstructor
 public class FilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void configure(HttpSecurity builder){
+        JwtTokenFilter jwtTokenFilter = new JwtTokenFilter(jwtTokenProvider);
+        builder.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
 }
---
+++ src/main/java/org/testboard/plyma_backend/global/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package org.testboard.plyma_backend.global.config;
+
+import io.jsonwebtoken.Jwt;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.testboard.plyma_backend.global.error.AuthenticationEntryPointImpl;
+import org.testboard.plyma_backend.global.jwt.JwtTokenProvider;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthenticationEntryPointImpl authenticationEntryPoint;
+
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider,
+                          AuthenticationEntryPointImpl authenticationEntryPoint){
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){return new BCryptPasswordEncoder();}
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.
+                formLogin().disable()
+                .cors().and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .httpBasic().disable()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/user/*").permitAll()
+                        .requestMatchers("/post/search").permitAll()
+                        .requestMatchers(HttpMethod.HEAD, "/user").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .apply(new FilterConfig(jwtTokenProvider));
+        return http.build();
+    }
+}
