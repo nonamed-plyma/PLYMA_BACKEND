90d10cedad8dad36ed3f0fdfc621e1dac03f528c
rudwk <tlsruwk12345@gmail.com>
2024-11-12 19:47:22 +0900

    📑 :: JwtTokenFilter추가

--- src/main/java/org/testboard/plyma_backend/global/jwt/JwtTokenFilter.java
+++ src/main/java/org/testboard/plyma_backend/global/jwt/JwtTokenFilter.java
@@ -1,4 +1,38 @@
 package org.testboard.plyma_backend.global.jwt;
 
-public class JwtTokenFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.apache.http.protocol.HTTP;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+    private static final String HEADER = "Authorization";
+    private static final String PREFIX = "Bearer";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest reques, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        String token = resolveToken(reques);
+
+        if(token != null && jwtTokenProvider.validateToken(token)){
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        chain.doFilter(reques, response);
+    }
+
+    private String resolveToken(HttpServletRequest request){
+        String bearnerToken = request.getHeader(HEADER);
+
+        if (bearnerToken != null && bearnerToken.startsWith(PREFIX)) return bearnerToken.substring(7);
+        return null;
+    }
 }
